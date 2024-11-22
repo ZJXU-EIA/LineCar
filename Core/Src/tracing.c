9a870dd7e627e6fdb67e7743a08663cb71f97adb
Nymphaea <mail@supaku.cn>
2024-11-22 22:46:31 +0800

    [feat] 优化循迹逻辑

--- Core/Src/tracing.c
+++ Core/Src/tracing.c
@@ -35,6 +35,7 @@ char Left;
 char Right;
 char LeftSlight;
 char RightSlight;
+char Cross;
 
 /**
  * @brief 初始化灰度传感器的状态
@@ -48,6 +49,9 @@ void InitializeStates() {
     RightSlight = Gray_6_Value == GPIO_PIN_SET;
     Left = Gray_1_Value == GPIO_PIN_SET;
     Right = Gray_7_Value == GPIO_PIN_SET;
+    Cross = Gray_1_Value == GPIO_PIN_SET && Gray_2_Value == GPIO_PIN_SET && Gray_3_Value == GPIO_PIN_SET &&
+        Gray_4_Value == GPIO_PIN_SET && Gray_5_Value == GPIO_PIN_SET && Gray_6_Value == GPIO_PIN_SET &&
+        Gray_7_Value == GPIO_PIN_SET;
 }
 
 /**
@@ -60,28 +64,38 @@ void tracing() {
     int right_speed = 30;
     static int servo_angle; // 静态保存舵机角度
     static int prev_servo_angle = 90; // 静态保存上一个舵机角度
+    static int crossCount = 0;
 
-    if (Centre) {
-        // Move forward
-        left_speed = 30;
-        right_speed = 30;
-        if (prev_servo_angle == 55) {
-            servo_angle = 90 + 1; // 舵机向右旋转小角度
-        } else if (prev_servo_angle == 125) {
-            servo_angle = 90 - 1; // 舵机向左旋转小角度
-        } else {
-            servo_angle = 90; // 保持直行
+    if (Cross) {
+        crossCount++;
+    }
+
+    if (crossCount >= 14) {
+        left_speed = 0;
+        right_speed = 0;
+    } else {
+        if (Centre) {
+            // Move forward
+            left_speed = 30;
+            right_speed = 30;
+            if (prev_servo_angle == 55) {
+                servo_angle = 90 + 1; // Small angle to the right
+            } else if (prev_servo_angle == 125) {
+                servo_angle = 90 - 1; // Small angle to the left
+            } else {
+                servo_angle = 90; // Center
+            }
+        } else if (Left || LeftSlight) {
+            // Turn left or slight left adjustment
+            left_speed = Left ? 20 : 25;
+            right_speed = Left ? 40 : 35;
+            servo_angle = Left ? 55 : 75; // Set servo angle for left turn or slight left adjustment
+        } else if (Right || RightSlight) {
+            // Turn right or slight right adjustment
+            left_speed = Right ? 40 : 35;
+            right_speed = Right ? 20 : 25;
+            servo_angle = Right ? 125 : 105; // Set servo angle for right turn or slight right adjustment
         }
-    } else if (Left || LeftSlight) {
-        // 左转或向左微调
-        left_speed = Left ? 20 : 25;
-        right_speed = Left ? 40 : 35;
-        servo_angle = Left ? 55 : 75; // 设置舵机左转角度或左转小角度
-    } else if (Right || RightSlight) {
-        // 右转或向右微调
-        left_speed = Right ? 40 : 35;
-        right_speed = Right ? 20 : 25;
-        servo_angle = Right ? 125 : 105; // 设置舵机右转角度或右转小角度
     }
 
     prev_servo_angle = servo_angle; // 更新上一个舵机角度
